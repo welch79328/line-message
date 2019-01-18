a0622b2e5fb6fea02601610c40362da51ee2db0d
welch <welch@adwifi.com.tw>
2019-01-18 23:07:30 +0800

    news modify

--- reply.php
+++ reply.php
@@ -170,83 +170,82 @@ foreach ($obj['events'] as $event) {
         }
     }
 
-
-    if ($event['message']['text'] == '測試新聞') {
+	if ($event['message']['text'] == '測試新聞') {
+        $dogRand = array_rand($dogdb['dog'],1);
         $payload = [
             'replyToken' => $event['replyToken'],
             'messages' => [
-				[
+                [
 					"type" => "template",
 					"altText" => "this is a carousel template",
 					"template" => [
-						"type" => "carousel",
-						"columns" => [
-							[
-								"thumbnailImageUrl" => "https =>//example.com/bot/images/item1.jpg",
-								"imageBackgroundColor" => "#FFFFFF",
-								"title" => "this is menu",
-								"text" => "description",
-								"defaultAction" => [
+					  "type" => "carousel",
+					  "columns" => [
+						  [
+							"thumbnailImageUrl" => "https =>//example.com/bot/images/item1.jpg",
+							"imageBackgroundColor" => "#FFFFFF",
+							"title" => "this is menu",
+							"text" => "description",
+							"defaultAction" => [
+								"type" => "uri",
+								"label" => "View detail",
+								"uri" => "http =>//example.com/page/123"
+							],
+							"actions" => [
+								[
+									"type" => "postback",
+									"label" => "Buy",
+									"data" => "action=buy&itemid=111"
+								],
+								[
+									"type" => "postback",
+									"label" => "Add to cart",
+									"data" => "action=add&itemid=111"
+								],
+								[
 									"type" => "uri",
 									"label" => "View detail",
-									"uri" => "http =>//example.com/page/123"
-								],
-								"actions" => [
-									[
-										"type" => "postback",
-										"label" => "Buy",
-										"data" => "action=buy&itemid=111"
-									],
-									[
-										"type" => "postback",
-										"label" => "Add to cart",
-										"data" => "action=add&itemid=111"
-									],
-									[
-										"type" => "uri",
-										"label" => "View detail",
-										"uri" => "http =>//example.com/page/111"
-									]
+									"uri" => "http =>//example.com/page/111"
 								]
+							]
+						  ],
+						  [
+							"thumbnailImageUrl" => "https =>//example.com/bot/images/item2.jpg",
+							"imageBackgroundColor" => "#000000",
+							"title" => "this is menu",
+							"text" => "description",
+							"defaultAction" => [
+								"type" => "uri",
+								"label" => "View detail",
+								"uri" => "http =>//example.com/page/222"
 							],
-							[
-								"thumbnailImageUrl" => "https =>//example.com/bot/images/item2.jpg",
-								"imageBackgroundColor" => "#000000",
-								"title" => "this is menu",
-								"text" => "description",
-								"defaultAction" => [
+							"actions" => [
+								[
+									"type" => "postback",
+									"label" => "Buy",
+									"data" => "action=buy&itemid=222"
+								],
+								[
+									"type" => "postback",
+									"label" => "Add to cart",
+									"data" => "action=add&itemid=222"
+								],
+								[
 									"type" => "uri",
 									"label" => "View detail",
 									"uri" => "http =>//example.com/page/222"
-								],
-								"actions" => [
-									[
-										"type" => "postback",
-										"label" => "Buy",
-										"data" => "action=buy&itemid=222"
-									],
-									[
-										"type" => "postback",
-										"label" => "Add to cart",
-										"data" => "action=add&itemid=222"
-									],
-									[
-										"type" => "uri",
-										"label" => "View detail",
-										"uri" => "http =>//example.com/page/222"
-									]
 								]
 							]
-						],
-						"imageAspectRatio" => "rectangle",
-						"imageSize" => "cover"
+						  ]
+					  ],
+					  "imageAspectRatio" => "rectangle",
+					  "imageSize" => "cover"
 					]
 				]
-			]
+            ]
         ];
         $sedMessage = true;
     }
-
 
 	if ($sedMessage) {
 		// Make payload
