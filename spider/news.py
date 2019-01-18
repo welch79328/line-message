ede9ed96707895dbb2b896ab8301fa15c4153dcf
welch <welch@adwifi.com.tw>
2019-01-19 01:09:44 +0800

    spider news modify

--- spider/news.py
+++ spider/news.py
@@ -27,23 +27,23 @@ for news in news_list:
     value['href'] = href
     value['title'] = title
 
-    try:
-        # 記得更改想要下載到的位
-        file_name = int(time.time())+random.randint(0,1000)
-        value['img_url'] = str(file_name)+'.jpg'
-        urlretrieve(img_url, os.path.dirname(os.path.abspath(__file__))+'/newsfile/'+str(file_name)+'.jpg')
-        try:
+	try:
+		# 記得更改想要下載到的位
+		file_name = int(time.time())+random.randint(0,1000)
+		value['img_url'] = str(file_name)+'.jpg'
+		urlretrieve(img_url, os.path.dirname(os.path.abspath(__file__))+'/newsfile/'+str(file_name)+'.jpg')
+		try:
 			jsonFile = open(os.path.dirname(os.path.abspath(__file__))+"news.json","r")
 			fileContent = jsonFile.read()
 			new_dict = json.loads(fileContent)
 			#key = len(new_dict['news'])
 			new_dict['news'].setdefault(key,value)
 			key = key + 1
-        except:
-            print ('error')
+		except:
+			print ('error')
 
-        with open(os.path.dirname(os.path.abspath(__file__))+"news.json","w") as f:
-            json.dump(new_dict,f)
-            print("加载入文件完成...")
-    except:
-        print('{} {}_{}.jpg 下載失敗!')
+		with open(os.path.dirname(os.path.abspath(__file__))+"news.json","w") as f:
+			json.dump(new_dict,f)
+			print("加载入文件完成...")
+	except:
+		print('{} {}_{}.jpg 下載失敗!')
