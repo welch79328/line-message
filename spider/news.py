7cfe968902bd8076b1eda8405329c03fa2791588
welchchen <welch79328@gmail.com>
2019-02-14 10:27:54 +0800

    spider news modify

--- spider/news.py
+++ spider/news.py
@@ -36,18 +36,17 @@ for news in news_list:
 		value['img_url'] = str(file_name)+'.jpg'
 		if not os.path.exists(filepath+"/file/news/"):
 			os.makedirs(filepath+"/file/news/")
-            os.chmod(filepath+"/file/news/", 0o777)
+			os.chmod(filepath+"/file/news/", 0o777)
 
-        if not os.path.exists(filepath+"/json/news.json"): 
-        	open(filepath+"/json/news.json","w")
+		if not os.path.exists(filepath+"/json/news.json"): 
+			open(filepath+"/json/news.json","w")
 
 		urlretrieve(img_url, filepath+'/file/news/'+str(file_name)+'.jpg')
 		try:
-
 			jsonFile = open(filepath+"/json/news.json","r")
-			fileContent = jsonFile.read()
-			new_dict = json.loads(fileContent)
-			#key = len(new_dict['news'])
+			if jsonFile == False:
+				fileContent = jsonFile.read()
+				new_dict = json.loads(fileContent)
 			new_dict['news'].setdefault(key,value)
 			key = key + 1
 		except:
